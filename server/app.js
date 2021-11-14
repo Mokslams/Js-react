25116d9b665486e004cbe8436cf07d8eac2173c1
Edvinas Ramanauskas <edvinyzas.rr@gmail.com>
2021-11-14 12:12:56 +0200

    Not the end

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3003
+const express = require('express');
+const app = express();
+const port = 3003;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/hello/:vardas', (req, res) => {
@@ -27,5 +34,40 @@ app.get('/div/:a/:b', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`App is working on: http://localhost:${port}`)
+    console.log(`App is working on: http://localhost:${port}`)
+})
+
+app.post('/calculator', (req, res) => {
+
+    const d1 = parseFloat(req.body.d1);
+    const d2 = parseFloat(req.body.d2);
+    let answer;
+    let errMsg;
+    switch (req.body.action) {
+        case '+':
+            answer = d1 + d2;
+            break;
+        case '-':
+            answer = d1 - d2;
+            break;
+        case '*':
+            answer = d1 * d2;
+            break;
+        case '/':
+            if (0 === d2) {
+                errMsg = 'be sansu seni'
+            } else {
+                answer = d1 / d2;
+            }
+            break;
+            default: errMsg = 'Wtf';
+    }
+
+    res.json({ 
+        answer: answer,
+        errMsg: errMsg
+    });
+
+
+    // res.json({ answer: 777 })
 })
--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
 import axios from "axios";
-axios.get('http://127.0.0.1:5500/public/data.txt').
-then(res => {
-    console.log(res);
+
+// 
+
+
+window.addEventListener('DOMContentLoaded', () => {
+const alert =   document.querySelector('.alert-danger');
+const result =   document.querySelector('.alert-success');
+
+
+    document.querySelector('button').addEventListener('click', () => {
+        const data = {};
+        data.d1 = document.querySelector('#d1').value || 0;
+        data.d2 = document.querySelector('#d2').value || 0;
+        data.action = document.querySelector('#action').value;
+        axios.post('http://127.0.0.1:3003/calculator', data).
+            then(res => {
+                console.log(res);
+                if(res.data.errMsg) {
+                    alert.style.dislpay = 'block';
+                    result.style.dislpay = 'none';
+                    alert.innerHTML = res.data.errMsg;
+                }else {
+                    alert.style.dislpay = 'none';
+                    result.style.dislpay = 'block';
+                    result.innerHTML = res.data.answer;
+                }
+            })
+    })
+
+
 })
-
-
-console.log('Hello js');
